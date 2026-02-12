b00c1fe3f888d996aacecfb8fdd6b6a1702123fe
DeadPieGamer <felix@krog.dk>
2026-02-12 15:29:09 +0100

    arm bot and doap

--- js/writeAge.js
+++ js/writeAge.js
@@ -1,19 +1,20 @@
-var originalContent = $('#part-with-age').html();
+var originalContent = $("#part-with-age").html();
 var alteredContent = replaceAgeInString(originalContent);
-$('#part-with-age').html(alteredContent)
+$("#part-with-age").html(alteredContent);
 
-function replaceAgeInString(stringWithAge){
-    var birthday = new Date(2003, 9, 6, 14, 4)
-    stringWithAge = stringWithAge.replace('{age}', calculateAge(birthday))
-    return stringWithAge;
+function replaceAgeInString(stringWithAge) {
+  var birthday = new Date(2003, 9, 6, 14, 4);
+  stringWithAge = stringWithAge.replace("{age}", calculateAge(birthday));
+  return stringWithAge;
 }
 
 // Source - https://stackoverflow.com/a/24181701
 // Posted by testUserPleaseIgnore, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-02-08, License - CC BY-SA 4.0
 
- function calculateAge(birthday) { // birthday is a date
-   var ageDifMs = Date.now() - birthday;
-   var ageDate = new Date(ageDifMs); // miliseconds from epoch
-   return Math.abs(ageDate.getUTCFullYear() - 1970);
- }
+function calculateAge(birthday) {
+  // birthday is a date
+  var ageDifMs = Date.now() - birthday;
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
